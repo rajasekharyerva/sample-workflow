93172260da7047fc60b3047479d076fb6105a24a
Yerva Rajasekhar Reddy <ryerva@altimetrik.com>
2025-10-29 19:36:34 +0530

    Initial commit

---
+++ src/test/java/steps/MyStepDefs.java
@@ -0,0 +1,21 @@
+package steps;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepDefs {
+
+    @When("Sample When")
+    public void sampleWhen() {
+    }
+
+    @Given("Sample Given")
+    public void sampleGiven() {
+    }
+
+    @Then("Sample Then")
+    public void sampleThen() {
+    }
+}
---
+++ src/test/resources/features/sample.feature
@@ -0,0 +1,5 @@
+Feature: Sample Feature
+  Scenario: Sample Scenario
+    Given Sample Given
+    When Sample When
+    Then Sample Then
